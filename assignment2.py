7d29194a24aca4abbb7613c0ddf605b6e5792685
Esther Auma <estheraumaego@gmail.com>
2023-07-06 07:51:39 +0300

    assingment2.py has been added

---
+++ assignment2.py
@@ -0,0 +1,11 @@
+#Using print statement as a debugging tool.
+
+def sum(num1,num2):
+    num3 = num1 + num2
+    print(num3)
+
+sum(4,6)
+
+
+universities = ["KYU", "Makerere","Mbarara"]
+print(universities)
